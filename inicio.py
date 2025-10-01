d3ef210dcf2d9346ec7f0c4048613eef3702eb0d
CristianVargas19 <vargasmontiel2@gmail.com>
2025-10-01 13:40:38 -0600

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)  
+app = Flask(__name__)
 
 @app.route("/")
 def inicio():
-    return "<h1>HOLA CINÉFILO</h1>"
+    return render_template('index.html')
